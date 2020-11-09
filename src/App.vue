c6346e916158bee666691127ae4c03a819fd504e
Alyssa Crawford <acrawford@dminc.com>
2020-11-09 13:30:36 -0500

    added trival routing

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
-  <Header @toggle-clicked="showAsList = !showAsList" />
-  <List-holder :showAsList="showAsList" />
+  <router-view/>
 </template>
 
-<script>
-import Header from "./components/Header.vue";
-import ListHolder from "./components/ListHolder.vue";
-
-export default {
-  name: "App",
-  components: {
-    Header,
-    ListHolder,
-  },
-  data: () => {
-    return {
-      showAsList: false,
-    };
-  },
-};
-</script>
-
 <style>
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header @toggle-clicked="showAsList = !showAsList" />
+  <List-holder :showAsList="showAsList" />
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import ListHolder from "../components/ListHolder.vue";
+export default {
+  name: "Home",
+  components: {
+    Header,
+    ListHolder,
+  },
+  data: () => {
+    return {
+      showAsList: false,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
