de45cbf708efeae1bccd234889f64dc9b0665703
Alyssa Crawford <acrawford@dminc.com>
2020-11-20 10:07:19 -0500

    wip

--- src/components/Modals/DeleteModal.vue
+++ src/components/Modals/DeleteModal.vue
@@ -8,16 +8,19 @@
         @click="(e) => deleteConfirmed(e)"
       />
     </div>
+    <Message :messageInfo="{ msg: 'Hello', eror: true }" />
   </div>
 </template>
 
 <script>
 import Button from "primevue/button";
+import Message from "../UI/Message";
 
 export default {
   emits: ["delete-confirmed"],
   components: {
     Button,
+    Message,
   },
   props: {
     gameName: {
