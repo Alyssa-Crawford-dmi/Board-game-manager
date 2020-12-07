7217e7c6484ecd8a36f9dac6a7999ac573b8ee78
Alyssa Crawford <acrawford@dminc.com>
2020-12-07 12:20:56 -0500

    wip

--- src/views/playground.vue
+++ src/views/playground.vue
@@ -1,65 +1,21 @@
 <template>
-  <Button
-    type="button"
-    label="Toggle"
-    @click="toggle"
-    aria-haspopup="true"
-    aria-controls="overlay_menu"
-  />
-  <Menu id="overlay_menu" ref="menu" :model="items" :popup="true" />
+  <Button type="button" label="Toggle" @click="toggle" />
 </template>
 
 <script>
-import Menu from "primevue/menu";
 import Button from "primevue/button";
+import axios from "axios";
 
 export default {
   components: {
-    Menu,
     Button,
   },
   data() {
-    return {
-      items: [
-        {
-          label: "My wishlist",
-          command: () => {
-            console.log("Wishlist");
-          },
-        },
-        {
-          label: "Owned games",
-          command: () => {
-            console.log("Owned");
-          },
-        },
-        {
-          label: "Shared with me",
-          command: () => {
-            console.log("shared");
-          },
-        },
-        {
-          label: "Sign out",
-          command: () => {
-            console.log("Signout");
-          },
-          icon: "pi pi-user",
-        },
-      ],
-    };
+    return {};
   },
   methods: {
-    toggle(event) {
-      this.$refs.menu.toggle(event);
-    },
-    save() {
-      this.$toast.add({
-        severity: "success",
-        summary: "Success",
-        detail: "Data Saved",
-        life: 3000,
-      });
+    toggle() {
+      axios.post(`/api/tempFunc?name=Alyssa`);
     },
   },
 };
