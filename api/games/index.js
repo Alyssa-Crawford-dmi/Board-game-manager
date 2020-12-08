d32c04adfe1989498b40800471b6950ff59d7521
Alyssa Crawford <acrawford@dminc.com>
2020-12-08 14:11:08 -0500

    wip

--- api/games/index.js
+++ api/games/index.js
@@ -1,8 +1,16 @@
 module.exports = async function(context, req) {
+  const method = req.method.toLowerCase();
   const foundUser = context.bindings.userEntity;
-  if (foundUser) {
-    context.res = { body: { gamesList: foundUser.ownedIds } };
-    context.done();
+
+  switch (method) {
+    case "get":
+      if (foundUser) {
+        context.res = { body: { gamesList: foundUser.ownedIds } };
+        context.done();
+      }
+      context.done();
+
+    case "post":
+      context.done();
   }
-  context.done();
 };
